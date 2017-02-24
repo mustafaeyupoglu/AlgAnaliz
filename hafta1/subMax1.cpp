d0ff06f3e64ac7add25415a3bbf115055c87cce1
Mustafa Eyüpoğlu <mustafa.eyupoglu95@gmail.com>
2017-02-24 22:02:48 +0200

    SubMax1

---
+++ hafta1/subMax1.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+int maxSubSum(int liste[8])
+{
+    int maxSum=0;
+
+    int i,j,k;
+    for(i=0; i< sizeof(liste);i++){
+        
+        for ( j=0;j<sizeof(liste);j++){
+	    int thisSum=0
+            for( k=i;k<j;k++){
+                thisSum += liste[k];
+            }
+            if(thisSum > maxSum)
+            {
+                maxSum = thisSum;
+            }
+    }
+    }
+return maxSum;
+        }
+int main()
+{
+    int liste[]={4, -3, 5, -2, -1, 2, 6,-2};
+    cout<< maxSubSum(liste);
+    return 0;
+}
