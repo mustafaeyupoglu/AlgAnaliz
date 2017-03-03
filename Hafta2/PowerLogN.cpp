2ca718cf901259215ce7ba75294675c735c1cc8a
Mustafa Eyüpoğlu <mustafa.eyupoglu95@gmail.com>
2017-03-03 12:41:36 +0200

    Update PowerLogN.cpp

--- Hafta2/PowerLogN.cpp
+++ Hafta2/PowerLogN.cpp
@@ -2,22 +2,22 @@
 #include <iostream>
 
 using namespace std;
-long powerLog(int x, int n)
+long long powerLog(long long  x, long long  n)
 {
     if(n==0) return 1;
     if(n==1) return x;
     if(n%2==0)
     {
-       return powerLog(x*x,n/2);
+       return powerLog(x*(long long)x,n/2);
     }
     else
     {
-       return powerLog(x*x,n/2)*x;
+       return powerLog(x*(long long)x,n/2)*x;
     }
 }
 int main(int argc, char *argv[])
 {
-    long sonuc=powerLog(5,12);
+    long long  sonuc=powerLog(2,60);
     cout << sonuc ;
     system("PAUSE");
     return EXIT_SUCCESS;
